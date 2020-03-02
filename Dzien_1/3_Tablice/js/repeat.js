ddefccefa91fe61c67e242b7015377f450704e2e
Joachim Otto <joachim.otto@nokia.com>
2020-03-02 15:05:20 +0100

    corrected and some quastions

--- Dzien_1/3_Tablice/js/repeat.js
+++ Dzien_1/3_Tablice/js/repeat.js
@@ -5,60 +5,66 @@
 //Dopisz ciało funkcji w taki sposób, żeby zwracać ```true``` (wartość booleanowską), kiedy tablica ma
 //liczby rosnące (każda następna jest większa od poprzedniej), a ```false``` &ndash; jeżeli nie.
 // Pamiętaj, żeby użyć słowa kluczowego ```return``` do zwracania danych z funkcji.
-const isNumbersGrowing=(array)=> {
+const isNumbersGrowing = (array) => {
   for (var i = 0; i < array.length; i++) {
-    if (array[i] > array[i+1]){
+    if (array[i] > array[i + 1]) {
       return false
     }
-  }return true
+  } return true
 
 }
 
-console.log("tablica [1,2,3,4,5,6,7]  jest rosnąca (powinno zwrócić true) " + isNumbersGrowing([1,2,3,4,5,6,7]));
-console.log("tablica [1,2,10,4,5,6,7] nie jest rosnąca (powinno zwrócić false) " + isNumbersGrowing([1,2,10,4,5,6,7]));
-console.log("tablica [-5,-4,0,4,6,21]  jest rosnąca (powinno zwrócić true) " + isNumbersGrowing([-5,-4,0,4,6,21]));
-console.log("tablica [-1,2,-1,4,5,6,7] nie jest rosnąca (powinno zwrócić false) " + isNumbersGrowing([-1,2,-1,4,5,6,7]));
+console.log("tablica [1,2,3,4,5,6,7]  jest rosnąca (powinno zwrócić true) " + isNumbersGrowing([1, 2, 3, 4, 5, 6, 7]));
+console.log("tablica [1,2,10,4,5,6,7] nie jest rosnąca (powinno zwrócić false) " + isNumbersGrowing([1, 2, 10, 4, 5, 6, 7]));
+console.log("tablica [-5,-4,0,4,6,21]  jest rosnąca (powinno zwrócić true) " + isNumbersGrowing([-5, -4, 0, 4, 6, 21]));
+console.log("tablica [-1,2,-1,4,5,6,7] nie jest rosnąca (powinno zwrócić false) " + isNumbersGrowing([-1, 2, -1, 4, 5, 6, 7]));
 console.log("tablica [8]  jest rosnąca (powinno zwrócić true) " + isNumbersGrowing([8]));
 // ### Zadanie A2
 // Napisz dwie funkcje ```add(array)``` i ```multiply(array)```.
 // Obie mają przyjmować tylko jeden argument &ndash; tablicę. Następnie funkcja ```add```
 //  ma zsumować wszystkie liczby znajdujące się w tej tablicy (przy pomocy pętli **for**),
 //  a funkcja ```multiply``` ma pomnożyć wszystkie liczby znajdujące się w tablicy (przy pomocy pętli **for**).
-const add=(array)=>{
-  let result=0;
+const add2 = (array) => {
+  let result = 0;
   for (let i = 0; i < array.length; i++) {
-    result +=array[i]
-  }return result
+    result += array[i]
+  } return result
 
 }
-console.log(add([1,2,3,4,5])))
+console.log(add2([1, 2, 3, 4, 5]))
 //W tych zadaniach widać np siłę funkcji jak map, reduce, filter
 //to samo można zapisać jako:
 const add = (array) => array.reduce((a, b) => a + b);
 const multiply = (array) => array.reduce((a, b) => a * b);
 //dużo czystsze i w praktyce tak się to stosuje. 
 
-const multiply=(array)=>{
-  let result=1
+const multiply = (array) => {
+  let result = 1
   for (let i = 0; i < array.length; i++) {
-    result*=array[i]
+    result *= array[i]
   }
   return result
 }
-console.log(multiply([1,2,3,4]));
+console.log(multiply([1, 2, 3, 4]));
 // ### Zadanie A3
 // Napisz funkcję ```distFromAverage```, która ma przyjmować jako argument tablicę z liczbami.
 // Zwracana przez funkcję tablica ma przedstawiać w kazdej komórce różnicę między liczbą z danej komórki a
 //  średnią wartością tablicy. Dla uproszczenia możesz użyć funkcji z poprzedniego zadania.
 // // Np.
 // ```
-const distFromAverage=(array)=> {
-    let resul=[]
-    let average_value= Math.floor(add(array)/array.length)
-    for (let i = 0; i < array.length; i++) {
-      resul.push(Math.abs(array[i] - average_value))
-    }
-    return resul
+const distFromAverage2 = (array) => {
+  let resul = []
+  let average_value = Math.floor(add(array) / array.length)
+  for (let i = 0; i < array.length; i++) {
+    resul.push(Math.abs(array[i] - average_value))
+  }
+  return resul
+}
+
+const calculateArrayAverage = (array) => {
+  return Math.floor(add(array) / array.length)
+  //return arr.reduce((a,b) => a+b ,0)/ arr.lenght             /mozna to jakos madrzej napisac z reduce ?
+
 }
 
 //tu też fajnie wchodzi map
@@ -68,9 +74,9 @@ const distanceFromAverage = (array) => {
   return array.map((element) => Math.abs(element - averageValue));
 }
 
-console.log(distFromAverage([1,2,3,4,5,6,7]))// => [3,2,1,0,1,2,3] (średnia z tablicy wejściowej to 4)
-console.log(distFromAverage([1,1,1,1])); //=> [0,0,0,0] (średnia z tablicy wejściowej to 1)
-console.log(distFromAverage([2,8,3,7])) //=> [3,3,2,2] (średnia z tablicy wejściowej to 5)
+console.log(distFromAverage([1, 2, 3, 4, 5, 6, 7]))// => [3,2,1,0,1,2,3] (średnia z tablicy wejściowej to 4)
+console.log(distFromAverage([1, 1, 1, 1])); //=> [0,0,0,0] (średnia z tablicy wejściowej to 1)
+console.log(distFromAverage([2, 8, 3, 7])) //=> [3,3,2,2] (średnia z tablicy wejściowej to 5)
 // ```
 //
 // ## Część B &ndash; zadania do rozwiązania samodzielnego
@@ -81,49 +87,49 @@ console.log(distFromAverage([2,8,3,7])) //=> [3,3,2,2] (średnia z tablicy wejś
 // 2. Wypisz ostatni owoc w konsoli (skorzystaj z atrybutu ```length```).
 // 3. W pętli wypisz wszystkie owoce (skorzystaj z atrybutu ```length```).
 
-const last_fruit=(array)=>{
-  console.log(array[array.length-1])
+const last_fruit = (array) => {
+  console.log(array[array.length - 1])
   console.log(array.length);
 }
 
-const first_fruit=(array)=>{
+const first_fruit = (array) => {
   console.log(array[0]);
 }
-const fruits_foreach=(array)=>{
-  array.forEach(function(element,index,array)  {
+const fruits_foreach = (array) => {
+  array.forEach(function (element, index, array) {
     console.log(element);
-})
+  })
 }
 
-fruits_foreach(['apple','strawberry','pear','banana']);
-last_fruit(['apple','strawberry','pear','banana']);
-first_fruit(['apple','strawberry','pear','banana']);
+fruits_foreach(['apple', 'strawberry', 'pear', 'banana']);
+last_fruit(['apple', 'strawberry', 'pear', 'banana']);
+first_fruit(['apple', 'strawberry', 'pear', 'banana']);
 
 // ### Zadanie B2
 // Napisz funkcję ```printTable(array)```, która przyjmuje tylko jeden parametr &ndash; tablicę. Następnie przy pomocy
 //pętli **for** przeiteruj (przejdź) po każdym elemencie i wypisz każdy z nich w konsoli.
-const printTable=(array)=> {
+const printTable = (array) => {
   for (let i = 0; i < array.length; i++) {
     console.log(array[i])
   }
 
 }
-printTable(['apple','strawberry','pear','banana'])
+printTable(['apple', 'strawberry', 'pear', 'banana'])
 //
 // ### Zadanie B3
 // Napisz funkcję ```factors```, która ma przyjmować tylko jeden argument &ndash; liczbę, która musi być większa od **0**.
 // Funkcja ta ma zwracać tablicę zawierającą wszystkie dzielniki podanej liczby (w kolejności malejącej).
 // Jeżeli liczba jest mniejsza lub równa **0**, to funkcja ma zwracać pustą tablicę.
-const factors=(num)=> {
-  let result=[]
-  if (num <0) {
+const factors = (num) => {
+  let result = []
+  if (num < 0) {
     return []
-  } for (var i = 0; i <=num; i++) {
-      if (num % i ==0){
-        result.push(i)
-      }
-  }return result.sort(function(a,b){
-      return b-a
+  } for (var i = 0; i <= num; i++) {
+    if (num % i == 0) {
+      result.push(i)
+    }
+  } return result.sort(function (a, b) {
+    return b - a
   })
 
 }
@@ -137,19 +143,19 @@ console.log(factors(-4)) //=> []
 //tablicę zawierającą rosnące liczby całkowite.
 //Funkcja ta ma zwracać brakującą liczbę (przez co tablica skacze o dwa zamiast o jeden).
 //Jeżeli w tablicy nie ma brakujących liczb, to funkcja ma zwracać ```null```.
-const getMissingElement=(array)=> {
-  for (let i = 0; i < array.length-1; i++) {
+const getMissingElement = (array) => {
+  for (let i = 0; i < array.length - 1; i++) {
 
-  if (array[i]+1 !==array[i+1]) {
-    return array[i]+1
+    if (array[i] + 1 !== array[i + 1]) {
+      return array[i] + 1
+    }
   }
-}
   return null
 
 }
-console.log(getMissingElement([1,2,3,4,5,6,7])) //=> null
-console.log(getMissingElement([6,7,8,10,11,12,13,14,15])) //=> 9
-console.log(getMissingElement([-4,-3,-2,0,1,2,3,4])) //=> -1
+console.log(getMissingElement([1, 2, 3, 4, 5, 6, 7])) //=> null
+console.log(getMissingElement([6, 7, 8, 10, 11, 12, 13, 14, 15])) //=> 9
+console.log(getMissingElement([-4, -3, -2, 0, 1, 2, 3, 4])) //=> -1
 // ```
 //
 //
@@ -159,19 +165,20 @@ console.log(getMissingElement([-4,-3,-2,0,1,2,3,4])) //=> -1
 // * rok urodzenia najstarszej osoby i jej wiek,
 // * rok urodzenia najmłodszej osoby i jej wiek.
 // Użyj pętli do iteracji tablicy, ale postaraj się pobierać elementy metodą ```pop``` lub ```shift```.
-const getBirthDay=(array)=>{
-  const current_year=2020;
-  array.sort((a,b)=> b-a)  //czy tak to sie robi ?
+const getBirthDay = (array) => {
+  const current_year = 2020;
+  array.sort((a, b) => b - a)  //czy tak to sie robi ?
   console.log(array);
   let oldest = array.pop()
   let youngest = array.shift()
 
-  return {'Rok urodzenia najstarszej osoby': oldest,
-          'Wiek najstarszej osoboy': current_year- oldest + ' lat',
-          'Rok urodzenia najmlodszej osoby': youngest,
-          'Wiek najmlodszej osoby': current_year-youngest + ' lat'
+  return {
+    'Rok urodzenia najstarszej osoby': oldest,
+    'Wiek najstarszej osoboy': current_year - oldest + ' lat',
+    'Rok urodzenia najmlodszej osoby': youngest,
+    'Wiek najmlodszej osoby': current_year - youngest + ' lat'
 
   }
 }
 
-console.log(getBirthDay([1991,2000,1968,2004,1999]));
+console.log(getBirthDay([1991, 2000, 1968, 2004, 1999]));
--- Dzien_1/4_Tablice_wielowymiarowe/js/repeat.js
+++ Dzien_1/4_Tablice_wielowymiarowe/js/repeat.js
@@ -12,13 +12,13 @@
 // Napisz funkcję ```checkArray``` przyjmującą jako argument tablicę dwuwymiarową z liczbami całkowitymi.
 // Funkcja ma zwrócić nową tablicę, w której kolejnymi elementami będą wartości boolean ```true``` lub ```false```, zależne od tego,
 // czy każdy element tablicy drugiego wymiaru jest parzysty.
-const checkArray=(array)=>{
-  let result=[];
+const checkArray2 = (array) => {
+  let result = [];
   for (let i = 0; i < array.length; i++) {
-    let isOdd=array[i].some((element)=> element %2 !==0) // czy najpierw powinienem zainicjowac zmienna isOdd ? Tutaj odrazu przypisanie do wartosci
+    let isOdd = array[i].some((element) => element % 2 !== 0) // czy najpierw powinienem zainicjowac zmienna isOdd ? Tutaj odrazu przypisanie do wartosci
     result.push(!isOdd)
 
-  }return result
+  } return result
 
 }
 
@@ -27,20 +27,23 @@ const isOdd = (number) => number % 2 === 0;
 const allArrayItemsAreOdd = (array) => array.every((element) => isOdd(element));
 
 const checkArray = (twoDimensionArray) => {
-  return twoDimensionArray.map((array) => allArrayItemsAreOdd(array))
+  return twoDimensionArray.map((array) => allArrayItemsAreOdd(array))             // o wiele bardziej czytelniejsze
 };
-  
+
+
+
+
 
 
 // ```JavaScript
 var arr = [
-[11, 12],
-[42, 2],
-[-4, -120],
-[0, 0],
-[1, 34],
+  [11, 12],
+  [42, 2],
+  [-4, -120],
+  [0, 0],
+  [1, 34],
 ];
-// console.log(checkArray(arr));
+console.log(checkArray(arr));
 //
 // output
 //
@@ -55,18 +58,18 @@ var arr = [
 // ### Zadanie B1
 // Napisz funkcję ```print2DArray```, która jako argument przyjmuje tablicę dwuwymiarową.
 //Następnie funkcja ta ma wypisać w konsoli zawartość tej tablicy.
-const print2DArray=(array)=> {
+const print2DArray = (array) => {
   for (let i = 0; i < array.length; i++) {
     array[i].forEach((element) =>
-          console.log(element))
+      console.log(element))
   }
 }
 print2DArray([
-[11, 12],
-[42, 2],
-[-4, -120],
-[0, 0],
-[1, 34],
+  [11, 12],
+  [42, 2],
+  [-4, -120],
+  [0, 0],
+  [1, 34],
 ])
 //
 // ### Zadanie B3
@@ -76,15 +79,24 @@ print2DArray([
 // Nastepnie użyj rozwiązania z zadania B1 do wypisania tej tablicy w konsoli.
 // Hint: Użyj petli zagnieżdżonych.
 
-const create2DArray=(rows,columns)=> {
-  let table=[]
+const create2DArray = (rows, columns) => {
+  let table = []
   for (let i = 0; i < rows; i++) {
-    table[i]=[]                              //posilkowalem sie roziwazaniami z neta (petla w petli troche jest to dla mnie abstrakcja :D).
-                                            // co oznacza to table[i]=[] ? Czy to jest inijacja drugiego wymariu ?
-     for (let j = 0; j < columns; j++) {    //Generalnie staraj się unikać pętli w pętli. Im więcej zagnieżdżeń tym gorzej
+    table[i] = []                              //posilkowalem sie roziwazaniami z neta (petla w petli troche jest to dla mnie abstrakcja :D).
+    // co oznacza to table[i]=[] ? Czy to jest inijacja drugiego wymariu ?
+    for (let j = 0; j < columns; j++) {    //Generalnie staraj się unikać pętli w pętli. Im więcej zagnieżdżeń tym gorzej
       table[i][j] = i + ',' + j             //Spada czytelność kodu, sam widzisz, że ciężko jest to skumać.
     }                                       // table[i]=[] oznacza, że w table tworzysz na pozycji [i] pustą tabelę. Po stworzeniu pustej 
-  }return table                           //tabeli zasilasz ją wartościami
+  } return table                           //tabeli zasilasz ją wartościami
 }                 //generalnie straszny antypattern :D Rozbiłbym to na mniejsze funkcje
 
-console.log(create2DArray(5,5));
+console.log(create2DArray(5, 5));
+
+const createArray = (rows) => {
+  let array=[]
+  for (let i = 0; i < rows; i++) {
+    array.map((element) => array[i])
+    
+  }return array
+ }
+ console.log(array(3))
